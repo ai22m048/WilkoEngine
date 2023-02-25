8019c8c3368b3129eecb63427e789e584515c6e9
Wilko Schröter <wschroeter@mywarm.at>
2023-02-25 23:52:46 +0100

    Setup Engine Logging.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WilkoEngine
+
 Game Engine
+
+
--- WilkoEngine/EntryPoint.h
+++ WilkoEngine/EntryPoint.h
@@ -6,7 +6,11 @@ extern WilkoEngine::Application* WilkoEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Wilko Engine\n");
+	// printf("Wilko Engine\n");
+	WilkoEngine::Log::Init();
+	WE_CORE_WARN("Initialized log!");
+	int a = 5;
+	WE_INFO("Hello! Var={0}", a);
 	auto app = WilkoEngine::CreateApplication();
 	app->Run();
 	delete app;
