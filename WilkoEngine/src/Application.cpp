c6be2e5d5cfbc5dcaf0298465fa5fead56cfb28f
Wilko Schröter <wschroeter@mywarm.at>
2023-03-16 18:46:42 +0100

    Dispatching events

--- WilkoEngine/src/Application.cpp
+++ WilkoEngine/src/Application.cpp
@@ -1,24 +1,33 @@
 #include "wepch.h"
 #include "Application.h"
 
-#include "Events/ApplicationEvent.h"
 #include "Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace WilkoEngine {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
 
-	void Application::Run() 
+		WE_CORE_TRACE("{0}", e);
+	}
+
+	void Application::Run()
 	{
 		while (m_Running)
 		{
@@ -26,6 +35,12 @@ namespace WilkoEngine {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
--- WilkoEngine/src/Application.h
+++ WilkoEngine/src/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace WilkoEngine {
@@ -11,8 +13,13 @@ namespace WilkoEngine {
 	public:
 		Application();
 		virtual ~Application();
+
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
