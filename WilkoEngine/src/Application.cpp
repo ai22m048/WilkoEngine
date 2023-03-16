f73b2f3a9e13ccc6f637e1628a969c440dd6301e
Wilko Schröter <wschroeter@mywarm.at>
2023-03-16 18:23:48 +0100

    glfw

--- WilkoEngine/src/Application.cpp
+++ WilkoEngine/src/Application.cpp
@@ -8,19 +8,24 @@
 
 namespace WilkoEngine {
 
+	Application::Application()
+	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
+	}
+
+	Application::~Application()
+	{
+	}
+
+
 	void Application::Run() 
 	{
-		WindowResizeEvent e(1280, 720);
-		if (e.IsInCategory(EventCategoryApplication))
+		while (m_Running)
 		{
-			WE_TRACE(e);
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
 		}
-		if (e.IsInCategory(EventCategoryInput))
-		{
-			WE_TRACE(e);
-		}
-
-		while (true);
 	}
 
 }
--- WilkoEngine/src/Application.h
+++ WilkoEngine/src/Application.h
@@ -2,13 +2,19 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Window.h"
 
 namespace WilkoEngine {
 
 	class WILKO_API Application
 	{
 	public:
+		Application();
+		virtual ~Application();
 		void Run();
+	private:
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 	// To be defined in CLIENT
